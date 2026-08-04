c9f6a6e770e6fcbc2eff89969cb5bf08c086025a
MatthewBuckle <matthewb@maveric-systems.com>
2026-08-04 16:11:55 +0530

    Added interface for AccountService

---
+++ src/main/java/com/banking/transaction_processor/service/AccountService.java
@@ -0,0 +1,9 @@
+package com.banking.transaction_processor.service;
+
+import com.banking.transaction_processor.dto.AccountResponse;
+import com.banking.transaction_processor.dto.CreateAccountRequest;
+
+public interface AccountService {
+
+    public AccountResponse createAccount(CreateAccountRequest createAccountRequest);
+}
