ea850864eddfc100bde8ca890e10794dcb74a518
MatthewBuckle <matthewb@maveric-systems.com>
2026-08-04 16:02:14 +0530

    DTO Validation for Account request

--- src/main/java/com/banking/transaction_processor/dto/CreateAccountRequest.java
+++ src/main/java/com/banking/transaction_processor/dto/CreateAccountRequest.java
@@ -1,11 +1,16 @@
 package com.banking.transaction_processor.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+
 
 @Data
 public class CreateAccountRequest {
 
+    @NotBlank(message = "Name cannot be empty")
     private String accountHolderName;
+
+    @NotBlank(message = "Email cannot be empty")
     private String email;
 
 }
