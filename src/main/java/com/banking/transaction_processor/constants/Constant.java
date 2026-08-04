ba084ca8f5e650dadc89bd93ad1125479519f4d7
MatthewBuckle <matthewb@maveric-systems.com>
2026-08-04 15:37:36 +0530

    Added Constants for reuse

---
+++ src/main/java/com/banking/transaction_processor/constants/Constant.java
@@ -0,0 +1,20 @@
+package com.banking.transaction_processor.constants;
+
+public class Constant {
+
+    public static final String ACCOUNT_CREATED = "Account created successfully";
+    public static final String ACCOUNT_BALANCE = "Account Balance";
+    public static final String DEPOSIT_SUCCESSFUL = "Deposit Successful";
+    public static final String WITHDRAW_SUCCESSFUL = "Withdraw Successful";
+    public static final String TRANSFER_SUCCESSFUL = "Transfer successful";
+    public static final String TRANSACTION_HISTORY = "Transaction history retrieved";
+    public static final String ACCOUNT_NOT_FOUND= "Account not found";
+    public static final String INSUFFICIENT_FUNDS = "Insufficient funds";
+    public static final String WITHDRAW = "Withdraw";
+    public static final String DEPOSIT = "Deposit";
+    public static final String TRANSFER_IN = "Transfer In";
+    public static final String TRANSFER_OUT = "Transfer Out";
+    public static final String INVALID_AMOUNT = "Invalid Amount";
+    public static final String VALIDATION_FAILED = "Validation Failed";
+    public static final String INVALID_DATE = "fromDate cannot be greater than toDate";
+}
