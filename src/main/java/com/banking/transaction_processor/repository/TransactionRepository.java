b5dcaeb2eb62ad29715d5aae41b039451de342b8
MatthewBuckle <matthewb@maveric-systems.com>
2026-08-04 16:45:58 +0530

    Transaction repository to return transaction history with date range

--- src/main/java/com/banking/transaction_processor/repository/TransactionRepository.java
+++ src/main/java/com/banking/transaction_processor/repository/TransactionRepository.java
@@ -4,9 +4,11 @@ package com.banking.transaction_processor.repository;
 import com.banking.transaction_processor.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     List<Transaction> findByAccount_IdOrderByTimestampDesc(Long accountId);
+    List<Transaction> findByAccount_IdAndTimestampBetweenOrderByTimestampDesc(Long accountId, LocalDateTime fromDateTime, LocalDateTime toDateTime);
 }
