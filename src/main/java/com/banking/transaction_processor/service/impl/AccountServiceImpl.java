8ed5b44cbd85173142cbc582b91e4e03e87223c0
MatthewBuckle <matthewb@maveric-systems.com>
2026-08-04 16:13:19 +0530

    API service implementation of Create account API

---
+++ src/main/java/com/banking/transaction_processor/service/impl/AccountServiceImpl.java
@@ -0,0 +1,32 @@
+package com.banking.transaction_processor.service.impl;
+
+import com.banking.transaction_processor.constants.Constant;
+import com.banking.transaction_processor.dto.AccountResponse;
+import com.banking.transaction_processor.dto.CreateAccountRequest;
+import com.banking.transaction_processor.entity.Account;
+import com.banking.transaction_processor.exception.AccountNotFoundException;
+import com.banking.transaction_processor.repository.AccountRepository;
+import com.banking.transaction_processor.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class AccountServiceImpl implements AccountService {
+
+    private final AccountRepository accountRepository;
+
+    public AccountResponse createAccount(CreateAccountRequest createAccountRequest) {
+        Account account = Account.builder().
+                accountHolderName(createAccountRequest.getAccountHolderName()).
+                email(createAccountRequest.getEmail()).
+                balance(BigDecimal.valueOf(0.00)).build();
+
+        accountRepository.save(account);
+        return AccountResponse.builder()
+                .accountHolderName(account.getAccountHolderName())
+                .email(account.getEmail()).build();
+    }
+}
