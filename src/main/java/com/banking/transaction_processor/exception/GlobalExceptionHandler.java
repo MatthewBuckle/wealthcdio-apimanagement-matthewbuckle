038baa7033a656c6be4e4cff3a93af39541db170
MatthewBuckle <matthewb@maveric-systems.com>
2026-08-04 15:49:10 +0530

    Added GlobalExceptionHandler class

---
+++ src/main/java/com/banking/transaction_processor/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.banking.transaction_processor.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
+
